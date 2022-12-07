845eac10276b8a3666662520914ae99055bcfbc7
Cornelia Gherman <70709834+corneliag08@users.noreply.github.com>
2022-12-07 11:40:51 +0300

    Update triere pr.py

--- triere pr.py
+++ triere pr.py
@@ -25,20 +25,31 @@ print(sum)
 
 #2
 from random import randint
-m=randint(0,1000)
-n=randint(0,1000)
+#puteti schimba intervalul 
+m=randint(0,100)
+n=randint(0,100)
 print("Primul nr :", m)
 print ("Al doilea nr :" , n)
-
+cond1=True
+cond2=True
 x=str(m)
 y=str(n)
 def probl(x1,y1):
     if x not in y :
         return False
-    while y not in x:
+    if y not in x:
         return False
     else:
         return True
 for a in x:
-    if a in y :
-        print ("Ele sunt prietene")
+    if a not in y :
+        cond1=False
+        break
+for a in y:
+    if a not in x:
+        cond2=False
+        break
+if(cond2 and cond1):
+    print("Ele sunt prietene")
+else :
+    print("Nu sunt prietene")
